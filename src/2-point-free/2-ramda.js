c0b97be7e6e0457678ce21f186534e847838a1d4
Martin89PL <marcingladkowski@gmail.com>
2020-07-19 12:21:57 +0200

    resolved: ramda solution

--- src/2-point-free/2-ramda.js
+++ src/2-point-free/2-ramda.js
@@ -1,4 +1,7 @@
+const R = require('ramda')
 /**
  * Write the same function from exercise 1, but this time use Ramda.
  */
-export const getShortNamedAnimals = null
+const isShorterThan = n => str => str.length < n;
+
+export const getShortNamedAnimals = R.filter(R.compose(isShorterThan(5), R.prop('name')));
